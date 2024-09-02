d41d15fdba8ca7c063bc602a2947167025bf9f20
MargueriteLaVache <mayeul.hospital@free.fr>
2024-09-02 23:49:50 +0200

    Made health bar reappear
    The health bars were not displaying because I had moved the texture so there was none in place. Now there are some.

--- Scenes/Character/character_class.gd
+++ Scenes/Character/character_class.gd
@@ -193,10 +193,10 @@ func _physics_process(delta) -> void:
 	# start animation, which is weird, and also it gets stuck on the transition from slope to lower ground.
 	# I think I should do nothing.
 	# Maybe I could rotate only a bit. Idk.
-	if is_on_floor() and sprite and hit_box:
-		var normal_angle: float = get_floor_normal().angle()
-		sprite.rotation = normal_angle + PI / 2
-		hit_box.rotation = normal_angle+ PI / 2
+	#if is_on_floor() and sprite and hit_box:
+		#var normal_angle: float = get_floor_normal().angle()
+		#sprite.rotation = normal_angle + PI / 2
+		#hit_box.rotation = normal_angle+ PI / 2
 		#if self is PlayerClass:
 			#print("normal_angle in c_c.gd: ", normal_angle)
 
